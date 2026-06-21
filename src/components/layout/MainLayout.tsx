b1817e87b9fe3fbe564c8521611e618076d6c190
Jay Sengar <jay@example.com>
2026-06-21 18:29:04 +0530

    perf: remove extremely laggy SVG noise overlay from layouts

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -15,15 +15,7 @@ export function MainLayout() {
     <div className="dark h-[100dvh] w-full bg-black text-foreground selection:bg-primary/30">
       <div className="mx-auto flex h-[100dvh] w-full max-w-[480px] flex-col bg-background shadow-[0_0_80px_-15px_rgba(0,0,0,0.5)] relative overflow-hidden">
         
-        {/* SVG Noise Overlay */}
-        <div className="pointer-events-none absolute inset-0 z-50 opacity-[0.03] mix-blend-overlay">
-          <svg className="h-full w-full opacity-50" xmlns="http://www.w3.org/2000/svg">
-            <filter id="noiseFilter">
-              <feTurbulence type="fractalNoise" baseFrequency="0.85" numOctaves="3" stitchTiles="stitch" />
-            </filter>
-            <rect width="100%" height="100%" filter="url(#noiseFilter)" />
-          </svg>
-        </div>
+
 
         <main className="flex-1 overflow-y-auto pb-28 relative z-0">
           <AnimatePresence mode="wait">
